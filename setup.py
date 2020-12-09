3e6cd9bc6e6c8feeb7f6e19a6044094c14a891be
yhs7710 <59632864+yhs7710@users.noreply.github.com>
2020-12-09 14:02:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="torchsummary",
+    name="torchsummary_mod",
     version="1.5.1",
     description="Model summary in PyTorch similar to `model.summary()` in Keras",
-    url="https://github.com/sksq96/pytorch-summary",
-    author="Shubham Chandel @sksq96",
-    author_email="shubham.zeez@gmail.com",
-    packages=["torchsummary"],
+    url="https://github.com/yhs7710/torchsummary_mod",
+    author="Yoon Hyeon-Seok @yhs7710",
+    author_email="yhs771000@gmail.com",
+    packages=["torchsummary_mod"],
 )
